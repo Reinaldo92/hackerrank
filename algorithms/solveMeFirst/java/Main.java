fdfe95e5023b07bb17e6cadf3761bcef71e3a45f
Reinaldo Jr <reinaldo.santos92@outlook.com>
2024-03-10 16:55:14 -0300

    java: solveMeFirst

---
+++ algorithms/solveMeFirst/java/Main.java
@@ -0,0 +1,26 @@
+package java;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT.
+         * Your class should be named Solution.
+         */
+
+        Scanner in = new Scanner(System.in);
+
+        int a;
+        int b;
+
+        a = in.nextInt();
+        b = in.nextInt();
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
